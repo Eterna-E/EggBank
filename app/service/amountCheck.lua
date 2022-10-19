dc0b4feae9cb6df1664e5aa1636dbb0ae3a12a65
ZhenHao <zhenhao@TangChuandeMacBook-Air-2.local>
2022-10-19 15:56:06 +0800

    修正高併發下資料一致性問題

--- app/service/amountCheck.lua
+++
@@ -1,9 +0,0 @@
-local userBalance = KEYS[1]
-local amount = tonumber(ARGV[1])
-local money = tonumber(redis.call('GET', userBalance))
-
-if (amount > money) then
-    return false
-end
-
-return true
